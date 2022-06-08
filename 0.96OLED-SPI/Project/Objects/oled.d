350f67c43ee34da67d4c13af6bef83c5c4fad0a3
nayooooo <97676318+nayooooo@users.noreply.github.com>
2022-06-09 01:20:47 +0800

    Add files via upload

---
+++ 0.96OLED-SPI/Project/Objects/oled.d
@@ -0,0 +1,41 @@
+.\objects\oled.o: ..\Hardware\OLED\oled.c
+.\objects\oled.o: ..\System\SYS\sys.h
+.\objects\oled.o: ..\User\stm32f10x.h
+.\objects\oled.o: ..\Core\core_cm3.h
+.\objects\oled.o: D:\programming\keil 5\ARM\ARMCC\Bin\..\include\stdint.h
+.\objects\oled.o: ..\User\system_stm32f10x.h
+.\objects\oled.o: ..\User\stm32f10x_conf.h
+.\objects\oled.o: ..\FWLIB\inc\stm32f10x_adc.h
+.\objects\oled.o: ..\User\stm32f10x.h
+.\objects\oled.o: ..\FWLIB\inc\stm32f10x_bkp.h
+.\objects\oled.o: ..\FWLIB\inc\stm32f10x_can.h
+.\objects\oled.o: ..\FWLIB\inc\stm32f10x_cec.h
+.\objects\oled.o: ..\FWLIB\inc\stm32f10x_crc.h
+.\objects\oled.o: ..\FWLIB\inc\stm32f10x_dac.h
+.\objects\oled.o: ..\FWLIB\inc\stm32f10x_dbgmcu.h
+.\objects\oled.o: ..\FWLIB\inc\stm32f10x_dma.h
+.\objects\oled.o: ..\FWLIB\inc\stm32f10x_exti.h
+.\objects\oled.o: ..\FWLIB\inc\stm32f10x_flash.h
+.\objects\oled.o: ..\FWLIB\inc\stm32f10x_fsmc.h
+.\objects\oled.o: ..\FWLIB\inc\stm32f10x_gpio.h
+.\objects\oled.o: ..\FWLIB\inc\stm32f10x_i2c.h
+.\objects\oled.o: ..\FWLIB\inc\stm32f10x_iwdg.h
+.\objects\oled.o: ..\FWLIB\inc\stm32f10x_pwr.h
+.\objects\oled.o: ..\FWLIB\inc\stm32f10x_rcc.h
+.\objects\oled.o: ..\FWLIB\inc\stm32f10x_rtc.h
+.\objects\oled.o: ..\FWLIB\inc\stm32f10x_sdio.h
+.\objects\oled.o: ..\FWLIB\inc\stm32f10x_spi.h
+.\objects\oled.o: ..\FWLIB\inc\stm32f10x_tim.h
+.\objects\oled.o: ..\FWLIB\inc\stm32f10x_usart.h
+.\objects\oled.o: ..\FWLIB\inc\stm32f10x_wwdg.h
+.\objects\oled.o: ..\FWLIB\inc\misc.h
+.\objects\oled.o: D:\programming\keil 5\ARM\ARMCC\Bin\..\include\stdbool.h
+.\objects\oled.o: ..\System\delay\delay.h
+.\objects\oled.o: ..\System\SYS\sys.h
+.\objects\oled.o: ..\Hardware\LED\led.h
+.\objects\oled.o: ..\Software\spi\spi.h
+.\objects\oled.o: ..\Hardware\OLED\oled.h
+.\objects\oled.o: D:\programming\keil 5\ARM\ARMCC\Bin\..\include\stdlib.h
+.\objects\oled.o: ..\Hardware\OLED\screen.h
+.\objects\oled.o: ..\Hardware\OLED\oledfont.h
+.\objects\oled.o: D:\programming\keil 5\ARM\ARMCC\Bin\..\include\math.h
---
+++ 0.96OLED-SPI/Project/Objects/spi.d
@@ -0,0 +1,39 @@
+.\objects\spi.o: ..\Software\spi\spi.c
+.\objects\spi.o: ..\System\SYS\sys.h
+.\objects\spi.o: ..\User\stm32f10x.h
+.\objects\spi.o: ..\Core\core_cm3.h
+.\objects\spi.o: D:\programming\keil 5\ARM\ARMCC\Bin\..\include\stdint.h
+.\objects\spi.o: ..\User\system_stm32f10x.h
+.\objects\spi.o: ..\User\stm32f10x_conf.h
+.\objects\spi.o: ..\FWLIB\inc\stm32f10x_adc.h
+.\objects\spi.o: ..\User\stm32f10x.h
+.\objects\spi.o: ..\FWLIB\inc\stm32f10x_bkp.h
+.\objects\spi.o: ..\FWLIB\inc\stm32f10x_can.h
+.\objects\spi.o: ..\FWLIB\inc\stm32f10x_cec.h
+.\objects\spi.o: ..\FWLIB\inc\stm32f10x_crc.h
+.\objects\spi.o: ..\FWLIB\inc\stm32f10x_dac.h
+.\objects\spi.o: ..\FWLIB\inc\stm32f10x_dbgmcu.h
+.\objects\spi.o: ..\FWLIB\inc\stm32f10x_dma.h
+.\objects\spi.o: ..\FWLIB\inc\stm32f10x_exti.h
+.\objects\spi.o: ..\FWLIB\inc\stm32f10x_flash.h
+.\objects\spi.o: ..\FWLIB\inc\stm32f10x_fsmc.h
+.\objects\spi.o: ..\FWLIB\inc\stm32f10x_gpio.h
+.\objects\spi.o: ..\FWLIB\inc\stm32f10x_i2c.h
+.\objects\spi.o: ..\FWLIB\inc\stm32f10x_iwdg.h
+.\objects\spi.o: ..\FWLIB\inc\stm32f10x_pwr.h
+.\objects\spi.o: ..\FWLIB\inc\stm32f10x_rcc.h
+.\objects\spi.o: ..\FWLIB\inc\stm32f10x_rtc.h
+.\objects\spi.o: ..\FWLIB\inc\stm32f10x_sdio.h
+.\objects\spi.o: ..\FWLIB\inc\stm32f10x_spi.h
+.\objects\spi.o: ..\FWLIB\inc\stm32f10x_tim.h
+.\objects\spi.o: ..\FWLIB\inc\stm32f10x_usart.h
+.\objects\spi.o: ..\FWLIB\inc\stm32f10x_wwdg.h
+.\objects\spi.o: ..\FWLIB\inc\misc.h
+.\objects\spi.o: D:\programming\keil 5\ARM\ARMCC\Bin\..\include\stdbool.h
+.\objects\spi.o: ..\System\delay\delay.h
+.\objects\spi.o: ..\System\SYS\sys.h
+.\objects\spi.o: ..\Hardware\LED\led.h
+.\objects\spi.o: ..\Software\spi\spi.h
+.\objects\spi.o: ..\Hardware\OLED\oled.h
+.\objects\spi.o: D:\programming\keil 5\ARM\ARMCC\Bin\..\include\stdlib.h
+.\objects\spi.o: ..\Hardware\OLED\screen.h
