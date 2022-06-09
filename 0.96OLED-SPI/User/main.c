3d95e12001455656bfb19c1dcccb50cfc296916d
nayooooo <97676318+nayooooo@users.noreply.github.com>
2022-06-10 02:56:25 +0800

    Update main.c

--- 0.96OLED-SPI/User/main.c
+++ 0.96OLED-SPI/User/main.c
@@ -13,22 +13,10 @@ int main()
 	
 	OLED_Clear();
 	
-//	OLED_Draw_Two_Vertical_Ellipses(63, 31, 30, 20, FILL); OLED_Refresh_Gram();
-//	OLED_Draw_Two_Vertical_Ellipses_Rotate(63, 31, 30, 20, 0, FILL); OLED_Refresh_Gram();
-//	OLED_Draw_Rounded_Cube(63, 31, 30, 60, 30, FILL);OLED_Refresh_Gram();
-//	OLED_Draw_Rounded_Cube(63, 31, 60, 30, 15, FILL);OLED_Refresh_Gram();
-//	OLED_Fill(0, 0, OLED_COL_MAX-1, OLED_ROW_MAX-1, FILL);
-	
 	LED = LED_ON;
 	
 	while(1)
 	{
-		OLED_Show_Rotating_Ellipse(63, 31, 30, 20, FILL);
-		OLED_Show_Rotating_Two_Vertical_Ellipses(63, 31, 30, 20, FILL);
-		OLED_Show_Rotating_Telescoping_Two_Vertical_Ellipses(63, 31, 30, 20, FILL);
-		OLED_Show_Sliding_Door(63, 31, 60, 30, FILL);
-		OLED_Show_Cube_to_Circle(63, 31, 60, 30, FILL);
-		OLED_Show_Cube_to_LikeCircle(63, 31, 60, 30, FILL);
-		OLED_Show_DNA(63, 31, 60, 30, FILL);
+		Demo_01();
 	}
 }
