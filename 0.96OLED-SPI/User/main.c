4b852ce0aa9ef935140ce667d877488ace1c63d4
nayooooo <97676318+nayooooo@users.noreply.github.com>
2022-06-11 19:52:42 +0800

    Add files via upload

---
+++ 0.96OLED-SPI/User/main.c
@@ -0,0 +1,22 @@
+#include "sys.h"
+#include <stdlib.h>
+#include "bmp.h"
+
+#define X_TEST 43
+#define Y_TEST 61
+
+int main()
+{
+	delay_init();
+	LED_Init();
+	OLED_SPI_Init();
+	
+	OLED_Clear();
+	
+	LED = LED_ON;
+	
+	while(1)
+	{
+		Demo_01();
+	}
+}
