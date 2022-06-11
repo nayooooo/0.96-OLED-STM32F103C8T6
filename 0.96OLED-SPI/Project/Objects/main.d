a720bea0c84becf768953a6d8427395376879e56
nayooooo <97676318+nayooooo@users.noreply.github.com>
2022-06-11 20:00:54 +0800

    Add files via upload

---
+++ 0.96OLED-SPI/Project/Objects/main.d
@@ -0,0 +1,40 @@
+.\objects\main.o: ..\User\main.c
+.\objects\main.o: ..\System\SYS\sys.h
+.\objects\main.o: ..\User\stm32f10x.h
+.\objects\main.o: ..\Core\core_cm3.h
+.\objects\main.o: D:\programming\keil 5\ARM\ARMCC\Bin\..\include\stdint.h
+.\objects\main.o: ..\User\system_stm32f10x.h
+.\objects\main.o: ..\User\stm32f10x_conf.h
+.\objects\main.o: ..\FWLIB\inc\stm32f10x_adc.h
+.\objects\main.o: ..\User\stm32f10x.h
+.\objects\main.o: ..\FWLIB\inc\stm32f10x_bkp.h
+.\objects\main.o: ..\FWLIB\inc\stm32f10x_can.h
+.\objects\main.o: ..\FWLIB\inc\stm32f10x_cec.h
+.\objects\main.o: ..\FWLIB\inc\stm32f10x_crc.h
+.\objects\main.o: ..\FWLIB\inc\stm32f10x_dac.h
+.\objects\main.o: ..\FWLIB\inc\stm32f10x_dbgmcu.h
+.\objects\main.o: ..\FWLIB\inc\stm32f10x_dma.h
+.\objects\main.o: ..\FWLIB\inc\stm32f10x_exti.h
+.\objects\main.o: ..\FWLIB\inc\stm32f10x_flash.h
+.\objects\main.o: ..\FWLIB\inc\stm32f10x_fsmc.h
+.\objects\main.o: ..\FWLIB\inc\stm32f10x_gpio.h
+.\objects\main.o: ..\FWLIB\inc\stm32f10x_i2c.h
+.\objects\main.o: ..\FWLIB\inc\stm32f10x_iwdg.h
+.\objects\main.o: ..\FWLIB\inc\stm32f10x_pwr.h
+.\objects\main.o: ..\FWLIB\inc\stm32f10x_rcc.h
+.\objects\main.o: ..\FWLIB\inc\stm32f10x_rtc.h
+.\objects\main.o: ..\FWLIB\inc\stm32f10x_sdio.h
+.\objects\main.o: ..\FWLIB\inc\stm32f10x_spi.h
+.\objects\main.o: ..\FWLIB\inc\stm32f10x_tim.h
+.\objects\main.o: ..\FWLIB\inc\stm32f10x_usart.h
+.\objects\main.o: ..\FWLIB\inc\stm32f10x_wwdg.h
+.\objects\main.o: ..\FWLIB\inc\misc.h
+.\objects\main.o: D:\programming\keil 5\ARM\ARMCC\Bin\..\include\stdbool.h
+.\objects\main.o: ..\System\delay\delay.h
+.\objects\main.o: ..\System\SYS\sys.h
+.\objects\main.o: ..\Hardware\LED\led.h
+.\objects\main.o: ..\Software\spi\spi.h
+.\objects\main.o: ..\Hardware\OLED\oled.h
+.\objects\main.o: D:\programming\keil 5\ARM\ARMCC\Bin\..\include\stdlib.h
+.\objects\main.o: ..\Hardware\OLED\screen.h
+.\objects\main.o: ..\Hardware\OLED\bmp.h
