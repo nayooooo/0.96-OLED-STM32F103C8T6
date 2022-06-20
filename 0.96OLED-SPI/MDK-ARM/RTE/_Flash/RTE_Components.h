d701c7af83cdb02fb56351e160fc0c0c11564667
nayooooo <97676318+nayooooo@users.noreply.github.com>
2022-06-21 00:48:12 +0800

    Add files via upload

---
+++ 0.96OLED-SPI/MDK-ARM/RTE/_Flash/RTE_Components.h
@@ -0,0 +1,20 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: '0.96OLED-SPI' 
+ * Target:  'Flash' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32f10x.h"
+
+
+#endif /* RTE_COMPONENTS_H */
